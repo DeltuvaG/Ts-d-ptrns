a72f492bb392c3bc43022199815d115c8b9ebf0d
DeltuvaG <g.deltuva@gop.link>
2018-05-25 10:11:33 +0200

    first example decorator patter initial commit

---
+++ decorator.ts
@@ -0,0 +1,14 @@
+class ModelS {
+    public description: string = "Model S";
+    public cost(): number {
+        return 73000;
+    } 
+}
+
+class ModelX {
+    public description: string = "Model X";
+
+    public cost(): number {
+        return 77000;
+    }
+}
